2bb57c5fdc94a36feb46241236f8497c6504ea76
xhette <margotcup@gmail.com>
2021-11-09 19:35:27 +0300

    Д/з 3ю5

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,6 @@
 <template>
 <section class="catalog">
-        <ul class="catalog__list">
-          <li class="catalog__item" v-for="(product, index) in products" :key="index">
-            <a class="catalog__pic" href="#">
-              <img :src="product.image" srcset="img/radio@2x.jpg 2x" :alt="product.title">
-            </a>
-
-            <h3 class="catalog__title">
-              <a href="#">
-                {{product.title}}
-              </a>
-            </h3>
-
-            <span class="catalog__price">
-              {{product.price}} ₽
-            </span>
-
-            <ul class="colors colors--black">
-              <li class="colors__item">
-                <label class="colors__label">
-                  <input class="colors__radio sr-only"
-                    type="radio" name="color-1" value="#73B6EA" checked="">
-                  <span class="colors__value" style="background-color: #73B6EA;">
-                  </span>
-                </label>
-              </li>
-              <li class="colors__item">
-                <label class="colors__label">
-                  <input class="colors__radio sr-only" type="radio" name="color-1" value="#8BE000">
-                  <span class="colors__value" style="background-color: #8BE000;">
-                  </span>
-                </label>
-              </li>
-              <li class="colors__item">
-                <label class="colors__label">
-                  <input class="colors__radio sr-only" type="radio" name="color-1" value="#222">
-                  <span class="colors__value" style="background-color: #222;">
-                  </span>
-                </label>
-              </li>
-            </ul>
-          </li>
-        </ul>
+  <ProductList :products="products"/>
 
         <ul class="catalog__pagination pagination">
           <li class="pagination__item">
@@ -97,9 +56,11 @@
 
 <script>
 import products from './data/products';
+import ProductList from './components/ProductList.vue';
 
 export default {
   name: 'App',
+  components: { ProductList },
   data() {
     return {
       products,
