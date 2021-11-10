e3bb78ce23182963aec93e730feff259ed9b41e7
xhette <margotcup@gmail.com>
2021-11-10 20:32:36 +0300

    3.5*

---
+++ src/components/ProductList.vue
@@ -0,0 +1,16 @@
+<template>
+     <ul class="catalog__list">
+          <li class="catalog__item" v-for="(product, index) in products" :key="index">
+            <ProductItem :product="product"/>
+          </li>
+        </ul>
+</template>
+
+<script>
+import ProductItem from './ProductItem.vue';
+
+export default {
+  components: { ProductItem },
+  props: ['products'],
+};
+</script>
